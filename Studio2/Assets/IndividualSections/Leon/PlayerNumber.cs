8174f40e3d3b49ad987c94cd1af60b898797f736
Leon <p.fernandes@saedubai.com>
2017-10-01 17:40:40 +0400

    message

---
+++ Studio2/Assets/IndividualSections/Leon/PlayerNumber.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNumber : MonoBehaviour {
+    public int NumberofPlayers;
+    public bool[] Players;
+    // Use this for initialization
+    void Start () {
+        Players = new bool[NumberofPlayers];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
