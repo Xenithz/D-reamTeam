4dff1ceff195e3a13f91cefc87d150f5fa772f36
AngeloG <amcgdev@gmail.com>
2017-10-02 18:51:39 +0400

    Added turning to character controller. To Do: Add jump and protective coding for physics based movement
    Signed-off-by: AngeloG <amcgdev@gmail.com>

--- Studio2/Assets/IndividualSections/Angelo/AngelosScripts/BombTagCharacterController.cs
+++ Studio2/Assets/IndividualSections/Angelo/AngelosScripts/BombTagCharacterController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(BoxCollider))]
 public class BombTagCharacterController : MonoBehaviour
 {
     #region PrivateVariables
@@ -16,6 +16,8 @@ public class BombTagCharacterController : MonoBehaviour
     #region PublicVariables
 
     public float desiredMovementSpeed;
+    public float desiredTurningSpeed;
+    public float desiredJumpForce;
     #endregion
 
     #region MyFunctions
@@ -35,8 +37,29 @@ public class BombTagCharacterController : MonoBehaviour
         test.x = test.x * desiredMovementSpeed;
         test.z = test.z * desiredMovementSpeed;
         test.y = 0f;
-        myRigidBody.AddForce(test, ForceMode.Force);
+        myRigidBody.AddForce(test, ForceMode.Impulse);
+    }
+
+    private void RotationExecution(Vector3 inputForRotation)
+    {
+        Vector3 test2 = gameObject.transform.up * inputForRotation.x;
+        test2.x = test2.x * desiredTurningSpeed;
+        myRigidBody.AddTorque(test2, ForceMode.VelocityChange);
+    }
+
+    private void JumpExecution()
+    {
+        if(myRigidBody.velocity.y == 0)
+        {
+            Debug.Log("LL");
+        }
+    }
+
+    private void DebugUtility(Vector3 inputForMovement)
+    {
+        Vector3 test = gameObject.transform.forward * inputForMovement.z + gameObject.transform.right * inputForMovement.x;
         Debug.DrawRay(gameObject.transform.position, test);
+        Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward);
     }
 
     #endregion
@@ -58,6 +81,8 @@ public class BombTagCharacterController : MonoBehaviour
     {
         Vector3 storageVector = MovementInput();
         MovementExecution(storageVector);
+        RotationExecution(storageVector);
+        DebugUtility(storageVector);
     }
 
     #endregion
