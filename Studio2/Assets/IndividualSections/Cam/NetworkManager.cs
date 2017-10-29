259ca02a2a8858c7cee461d3abd1097bc0944ffb
cameronnandez <cameron.jf@hotmail.co.uk>
2017-10-29 14:34:49 +0400

    test

--- Studio2/Assets/IndividualSections/Cam/NetworkManager.cs
+++ Studio2/Assets/IndividualSections/Cam/NetworkManager.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using Photon;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
+public enum GameStates
+{
+    Starting,
+    InProgress,
+    GameOver
+}
 public class NetworkManager : PunBehaviour {
+    public static NetworkManager Instance; 
+    [SerializeField]private Text connectionText;
+    [SerializeField]private GameObject player;
+    [SerializeField]private GameObject[] spawnpoints;
+    private int playerInRoom = 0; 
+ 
 
-    [SerializeField]private Text connectionText; 
 
-	// Use this for initialization
-	void Start () {
+    //[SerializeField]private GameObject lobbyCamera;
+
+    // Use this for initialization
+    void Start () {
 
         PhotonNetwork.ConnectUsingSettings("0.1");
 		
@@ -19,6 +33,13 @@ public class NetworkManager : PunBehaviour {
     public virtual void OnJoinedLobby()
     {
         Debug.Log("connected to master");
+        PhotonNetwork.JoinOrCreateRoom("New", null, null);
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        playerInRoom++; 
+        PhotonNetwork.Instantiate(player.name,spawnpoints[playerInRoom-1].transform.position, Quaternion.identity,0);
     }
 	// Update is called once per frame
 	void Update () {
