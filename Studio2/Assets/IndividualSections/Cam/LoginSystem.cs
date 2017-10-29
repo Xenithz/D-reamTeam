aa9ac37e84c01c03f2621038db03a4d9b598ecb3
cameronnandez <cameron.jf@hotmail.co.uk>
2017-10-29 12:36:35 +0400

    aaa

---
+++ Studio2/Assets/IndividualSections/Cam/LoginSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Photon;
+using UnityEngine;
+
+
+
+
+public class LoginSystem : PunBehaviour {
+    public string inputUsername;
+    public string inputPassword;
+    public Text userField;
+    public Text passwordField; 
+    public string loginUrl= "https://localhost/game/Login.php"; 
+
+	IEnumerator Login (string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+
+        WWW www = new WWW(loginUrl, form);
+        yield return www;
+
+        Debug.Log(www.text);
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(Login(inputUsername, inputPassword));
+        }
+        
+
+        
+    }
+
+    public void OnLogin()
+    {
+        StartCoroutine(Login(inputUsername, inputPassword));
+    }
+
+    public void GetUsername()
+    {
+        inputUsername = userField.text.ToString() ;
+
+    }
+
+    public void GetPassword()
+    {
+        inputPassword = passwordField.text.ToString();
+    }
+
+}
---
+++ Studio2/Assets/IndividualSections/Cam/Register.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon;
+using UnityEngine.UI; 
+using UnityEngine;
+
+
+public class Register : PunBehaviour {
+
+
+    public string userUrl = "http://localhost/game/register.php";
+    public string inputUserName;
+    public string inputPassword;
+    public string inputEmail;
+    public string inputConfirm;
+ 
+
+    public int UserID;
+
+    public Text userTextField;
+    public Text passwordTextField;
+    public Text emailTextField;
+    public Text passwordConfirmField;
+
+    public GameObject panel;
+
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+
+
+    public IEnumerator CreateUser(string username, string password, string email)
+    {
+        WWWForm userForm = new WWWForm();
+        userForm.AddField("usernamePost", username);
+        userForm.AddField("passwordPost", password);
+        userForm.AddField("emailPost", email);
+
+        WWW registerURL = new WWW(userUrl, userForm);
+
+        yield return registerURL;
+        Debug.Log(registerURL.text);
+    }
+   
+
+    public void OnClickRegister()
+    {
+        inputUserName = userTextField.text.ToString();
+        inputPassword = passwordTextField.text.ToString();
+        inputEmail = emailTextField.text.ToString();
+        inputConfirm = passwordConfirmField.text.ToString();
+
+        CheckPassword();
+
+    }
+
+    public void CheckPassword()
+    {
+
+        if (inputPassword == inputConfirm)
+        {
+            panel.GetComponent <Image>().color = Color.green;
+            StartCoroutine(CreateUser(inputUserName, inputPassword, inputEmail));
+        }
+
+        else
+        {
+            panel.GetComponent<Image>().color = Color.red;
+            Debug.Log("Passwords do not match");
+
+        }
+    }
+
+}
