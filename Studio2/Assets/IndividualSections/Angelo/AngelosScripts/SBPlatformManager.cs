949b5bea81e859c337a5c6d2467d8605efcb54f0
AngeloG <amcgdev@gmail.com>
2017-11-19 03:14:49 +0400

    lllll
    Signed-off-by: AngeloG <amcgdev@gmail.com>

--- Studio2/Assets/IndividualSections/Angelo/AngelosScripts/SBPlatformManager.cs
+++ Studio2/Assets/IndividualSections/Angelo/AngelosScripts/SBPlatformManager.cs
@@ -19,13 +19,16 @@ public class SBPlatformManager : MonoBehaviour
     public float timeBetweenPhases;
 
     public PlatformPhases currentPhase;
-
+    public MeshRenderer platform1Renderer, platform2Renderer, platform3Renderer;
+    public Material platform1Material, platform2Material, platform3Material;
 
     public GameObject platform;
 
+    public GameObject platform1, platform2, platform3;
     public Vector3 platform1FinalScale, platform2FinalScale, platform3FinalScale;
 
     public bool isReadyToSwitch;
+    public Color myColor;
 
     
 
@@ -34,28 +37,30 @@ public class SBPlatformManager : MonoBehaviour
 
         if (currentPhase == PlatformPhases.Phase1 && isReadyToSwitch == false)
         {
-            Shrink(platform1FinalScale);
+            ShrinkNoPlat(platform1FinalScale);
             
         }
 
         if (isReadyToSwitch == true && currentPhase == PlatformPhases.Phase1)
         {
             Timer(PlatformPhases.Phase2);
+            ColorLerp(platform2Material);
         }
 
         if (currentPhase == PlatformPhases.Phase2 && isReadyToSwitch == false)
         {
-            Shrink(platform2FinalScale);
+            Shrink(platform2FinalScale, platform2);
         }
 
         if (isReadyToSwitch == true && currentPhase == PlatformPhases.Phase2)
         {
             Timer(PlatformPhases.Phase3);
+            ColorLerp(platform3Material);
         }
 
         if (currentPhase == PlatformPhases.Phase3 && isReadyToSwitch == false)
         {
-            Shrink(platform3FinalScale);
+            Shrink(platform3FinalScale, platform3);
         }
 
         if (SumoNetworkManager.Instance.currentGameState == GameStates.InProgress)
@@ -77,6 +82,21 @@ public class SBPlatformManager : MonoBehaviour
         currentPhase = PlatformPhases.Phase0;
         //isReadyToSwitch = false;
 
+        platform1FinalScale = platform1.transform.localScale;
+        platform2FinalScale = platform2.transform.localScale;
+        platform3FinalScale = platform3.transform.localScale;
+        platform1Renderer = GetComponent<MeshRenderer>();
+        platform1Material = platform1Renderer.materials[1];
+        platform2Renderer = platform2.GetComponent<MeshRenderer>();
+        platform2Material = platform2Renderer.materials[1];
+        platform3Renderer = platform3.GetComponent<MeshRenderer>();
+        platform3Material = platform3Renderer.materials[1];
+        myColor = new Color(0, 165, 251, 255);
+    }
+
+    public void ColorLerp(Material materialToLerp)
+    {
+        materialToLerp.color = Color.Lerp(myColor, Color.red, Mathf.PingPong(Time.time, 1));
     }
 
     public void Timer(PlatformPhases platformPhaseToSwitchTo)
@@ -91,11 +111,20 @@ public class SBPlatformManager : MonoBehaviour
     }
 
     
-    public void Shrink(Vector3 scaleToShrinkTo)
+    public void Shrink(Vector3 scaleToShrinkTo, GameObject platformToDisable)
     {
-        platform.transform.localScale = Vector3.Lerp(platform.transform.localScale, scaleToShrinkTo, Time.deltaTime);
-
+        platform.transform.localScale = scaleToShrinkTo;
+        platformToDisable.SetActive(false);
         if(platform.transform.localScale == scaleToShrinkTo)
+        {
+            isReadyToSwitch = true;
+        }
+    }
+
+    public void ShrinkNoPlat(Vector3 scaleToShrinkTo)
+    {
+        platform.transform.localScale = scaleToShrinkTo;
+        if (platform.transform.localScale == scaleToShrinkTo)
         {
             isReadyToSwitch = true;
         }
