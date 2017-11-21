d42d0196f7c0b77cdc02cacfafa75b984371670b
Leon <p.fernandes@saedubai.com>
2017-11-21 11:23:00 +0400

    scroll

---
+++ Studio2/Assets/IndividualSections/Leon/Scripts/ScrollingBG.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBG : MonoBehaviour {
+    Vector3 pos;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos = transform.position;
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (pos.z <= -27.7)
+        {
+            pos.z = 7.7f;
+            transform.position = pos;
+        }
+    }
+}
+
+
+    
+   
