873ca80691a91299ea5279826b18ec79fb80dd13
AngeloG <amcgdev@gmail.com>
2017-11-28 14:56:08 +0400

    123
    Signed-off-by: AngeloG <amcgdev@gmail.com>

---
+++ Studio2/Assets/AMTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AMTest : MonoBehaviour
+{
+    public AudioSource myAudioSource;
+
+    private void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioManager.instance.PlaySFX(myAudioSource, 0);
+        }
+    }
+}
